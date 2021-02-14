90a89dcb2c945d6a40b4e3c2beab0310bfe63ff8
tactoe <tmilon@student.42.fr>
2021-02-14 23:31:41 +0100

    adding first draft

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3.MoveTowards(transform.position, transform.position + transform.forward * 10, Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+    }
+}
---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Position
+{
+    public int x;
+    public int y;
+}
+
+public class PlayerCharacter : MonoBehaviour
+{
+    private Position pos = new Position();
+
+    [SerializeField] private GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos.x = 0;
+        pos.y = 0;
+        UpdatePos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ArrowKeyPressed())
+        {
+            pos.x += (int)Input.GetAxisRaw("Horizontal");
+            pos.y += (int)Input.GetAxisRaw("Vertical");
+            UpdatePos();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var tmp = Instantiate(bulletPrefab);
+            tmp.transform.position = transform.position;
+            tmp.transform.forward = transform.forward;
+        }
+    }
+
+    void UpdatePos()
+    {
+        var transform1 = transform;
+        transform1.position = new Vector3(pos.x, transform1.position.y, pos.y);
+    }
+
+    bool ArrowKeyPressed()
+    {
+        return (Input.GetKeyDown(KeyCode.LeftArrow) ||
+                Input.GetKeyDown(KeyCode.RightArrow) ||
+                Input.GetKeyDown(KeyCode.UpArrow) ||
+                Input.GetKeyDown(KeyCode.DownArrow));
+    }
+}
