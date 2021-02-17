7491d3327094da047fcbd16bd61f632d685c41aa
tactoe <tmilon@student.42.fr>
2021-02-17 22:48:15 +0100

    Small fix for v0.1

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void Exit()
+    {
+        GameManager.Instance.ExitGame();
+    }
+
+    public void StartGame()
+    {
+        GameManager.Instance.NextScene();
+    }
+}
